cb61ad8d6ffc20bd64aecb4d515a134ea81426d6
Matt Davis <6775756+nitzmahone@users.noreply.github.com>
2022-08-04 18:41:16 -0700

    add support/tests for build_arg passthru (#7)
    * add support/tests for build_arg passthru
    
    * misc cleanups
    
    * ensure default build_args are present
    
    * minor reformatting/help text change
    
    * provide context on `black` failures

--- src/containmint.py
+++ src/containmint.py
@@ -14,6 +14,7 @@ import itertools
 import json
 import os
 import pathlib
+import re
 import secrets
 import shlex
 import shutil
@@ -63,6 +64,7 @@ class BuildCommand(Command, metaclass=abc.ABCMeta):
     push: bool
     login: bool
     squash: t.Optional[str]
+    build_args: t.Optional[list[str]]
 
 
 @dataclasses.dataclass(frozen=True)
@@ -86,6 +88,7 @@ class Build(BuildCommand):
             login=self.login,
             push=self.push,
             squash=self.squash,
+            build_args=self.build_args,
         )
 
         ansible_test_shell = ('ansible-test', 'shell', '--target-posix', f'remote:{self.remote},arch={self.arch}', '--color', '-v', '--truncate', '0')
@@ -206,6 +209,9 @@ class Execute(BuildCommand):
         elif engine.program.name == 'docker':
             if self.squash:
                 raise SquashUnsupportedError(squash_mode=self.squash, container_engine=engine.program.name)
+
+        for build_arg in self.build_args or []:
+            options.extend(('--build-arg', build_arg))
 
         with registry_login(image.server, credentials):
             engine.run('build', '--tag', self.tag, '--file', match, self.context, '--no-cache', *options)
@@ -643,6 +649,14 @@ def context_ref(value: str) -> str:
     return value
 
 
+def key_value(value: str) -> str:
+    """Validate an arg is of the form key=value."""
+    if not re.match(r'.+=.+', value):
+        raise argparse.ArgumentTypeError('key=value format is required')
+
+    return value
+
+
 def run_command(
     *command: str,
     data: str | None = None,
@@ -694,6 +708,7 @@ def parse_args() -> Command:
     common_build_parser.add_argument('--push', action='store_true', help='push the image')
     common_build_parser.add_argument('--no-login', action='store_false', dest='login', help='do not log in')
     common_build_parser.add_argument('--squash', choices=['new', 'all'], help='squash to a single layer (choices: %(choices)s)')
+    common_build_parser.add_argument('--build-arg', metavar='K=V', type=key_value, dest='build_args', action='append', help='build arg passed to the builder')
 
     build_parser = subparsers.add_parser(Build.cli_name(), parents=[common_build_parser], description=Build.__doc__, help=Build.__doc__)
     build_parser.add_argument('--keep-instance', action='store_true', help='keep the remote instance')
